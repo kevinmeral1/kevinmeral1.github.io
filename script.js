5ef9d9bbea2b9e7812b93a5c37242c8eee993483
kevinmeral1 <136506281+kevinmeral1@users.noreply.github.com>
2023-06-23 23:11:52 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -48,46 +48,46 @@ const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerH
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0xffffff); // Set the background color to white
-renderer.shadowMap.enabled = true; // Enable shadows
+renderer.setClearColor(0xffffff); 
+renderer.shadowMap.enabled = true; 
 
 document.getElementById('container').appendChild(renderer.domElement);
 
 const dodecahedronRadius = 0.1;
 const geometry = new THREE.DodecahedronGeometry(dodecahedronRadius);
 
-// Load the image texture
+
 const textureLoader = new THREE.TextureLoader();
 const texture = textureLoader.load('abstrakte-klare-metallische-hintergrundnahaufnahme.jpg');
 
-// Use the image texture in the material
+
 const material = new THREE.MeshStandardMaterial({ map: texture });
 
 const dodecahedron = new THREE.Mesh(geometry, material);
 
-// Position the dodecahedron
-dodecahedron.position.y; // Height of the dodecahedron (half of the edge length)
+
+dodecahedron.position.y; 
 dodecahedron.rotation.z;
 
-// Rotate the dodecahedron 90 degrees around the x-axis so that a face lies on the ground
 
-dodecahedron.castShadow = true; // Object casts shadows
+
+dodecahedron.castShadow = true; 
 scene.add(dodecahedron);
 
-const light = new THREE.DirectionalLight(0xffffff, 10); // White directional light
+const light = new THREE.DirectionalLight(0xffffff, 10); 
 light.position.set(10, 10, 10);
-light.castShadow = true; // Light casts shadows
+light.castShadow = true;
 scene.add(light);
 
-const ambientLight = new THREE.AmbientLight(0x404040); // Ambient background light
+const ambientLight = new THREE.AmbientLight(0x404040); 
 scene.add(ambientLight);
 
 const groundSize = 10;
 const ground = new THREE.Mesh(
   new THREE.PlaneGeometry(10, 10),
-  new THREE.ShadowMaterial({ opacity: 0.3 }) // Material for the ground with shadows
+  new THREE.ShadowMaterial({ opacity: 0.3 }) 
 );
-ground.rotation.x = -Math.PI / 2; // Rotate the ground 90 degrees along the x-axis
+ground.rotation.x = -Math.PI / 2; 
 ground.receiveShadow = true;
 scene.add(ground);
 
@@ -96,14 +96,14 @@ camera.position.z = dodecahedronRadius * 3;
 function animate() {
   requestAnimationFrame(animate);
 
-  // Check screen size and update scale
+
   if (window.innerWidth < 480) {
-    dodecahedron.scale.set(0.8, 0.8, 0.8); // Scale the dodecahedron smaller
+    dodecahedron.scale.set(0.8, 0.8, 0.8); 
   } else {
-    dodecahedron.scale.set(1, 1, 1); // Reset the scale to its original size
+    dodecahedron.scale.set(1, 1, 1); 
   }
 
-  dodecahedron.rotation.y += 0.01; // Horizontal rotation angle
+  dodecahedron.rotation.y += 0.01; 
 
   renderer.render(scene, camera);
 }
@@ -118,7 +118,6 @@ function resizeContainer() {
   var windowWidth = window.innerWidth;
   var windowHeight = window.innerHeight;
 
-  // Hier können Sie die maximalen Breite und Höhe anpassen, bei denen der Container skaliert werden soll
   var maxWidth = 400;
   var maxHeight = 1800;
 
@@ -131,9 +130,9 @@ function resizeContainer() {
 
 window.addEventListener('resize', resizeContainer);
 
-// Initialisieren der Skalierung beim Laden der Seite
+
 window.addEventListener('load', resizeContainer);
-// Schaltflächen referenzieren
+
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
 const button3 = document.getElementById('button3');
@@ -141,51 +140,45 @@ const button4 = document.getElementById('button4');
 
 // Funktionen für Schaltflächen
 button1.addEventListener('click', function() {
-  dodecahedron.material.color.set(0x1c1c1c); // Setze die Farbe des Dodekaeders auf Schwarz
+  dodecahedron.material.color.set(0x1c1c1c); 
 });
 
 button2.addEventListener('click', function() {
-  dodecahedron.material.color.set(0x999999); // Setze die Farbe des Dodekaeders auf Weiß
+  dodecahedron.material.color.set(0x999999); 
 });
 
 button3.addEventListener('click', function() {
-  dodecahedron.material.color.set(0x1FB01C); // Setze die Farbe des Dodekaeders auf Grün
+  dodecahedron.material.color.set(0x1FB01C); 
 });
 
 button4.addEventListener('click', function() {
-  dodecahedron.material.color.set(0x037CC2); // Setze die Farbe des Dodekaeders auf Blau
+  dodecahedron.material.color.set(0x037CC2); 
 });
 
 
 // Video automatisches Abspielen
 
-// Funktion, um zu überprüfen, ob ein Element im sichtbaren Bereich ist
-function isElementInViewport(el) {
-  var rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-
-// Überwache das Scrollereignis
-window.addEventListener('scroll', function() {
-  var video = document.querySelector('.video-container video');
-  
-  // Überprüfe, ob das Video im sichtbaren Bereich ist
-  if (isElementInViewport(video)) {
-    // Prüfe, ob das Video bereits abgespielt wurde
-    if (video.currentTime === 0 || video.paused) {
-      // Spiele das Video ab, wenn es im sichtbaren Bereich ist und nicht bereits abgespielt wird
-      video.play();
+ function isElementInViewport(el) {
+      var rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
     }
-  } else {
-    // Pausiere das Video, wenn es außerhalb des sichtbaren Bereichs ist
-    video.pause();
-  }
-});
+
+    window.addEventListener('scroll', function() {
+      var video = document.querySelector('.video-container video');
+  
+      if (isElementInViewport(video)) {
+        if (video.currentTime === 0 || video.paused) {
+          video.play();
+        }
+      } else {
+        video.pause();
+      }
+    });
 
 
 
@@ -208,27 +201,27 @@ window.addEventListener('scroll', function() {
 
 
 // Tabelle Animation
-const targetSize = 100; // Zielgröße für die Animation
-let currentSize = 0; // Aktuelle Größe
-let animationRunning = false; // Variable zur Verfolgung des Animationsstatus
+const targetSize = 100; 
+let currentSize = 0; 
+let animationRunning = false; 
 
 function animateSize() {
   if (currentSize < targetSize) {
     currentSize++;
 
-    // Geschwindigkeitsanpassung basierend auf dem aktuellen Wert
-    const speed = Math.max(0.1, 1 - currentSize / targetSize); // Je größer der Wert, desto langsamer wird die Animation
+    
+    const speed = Math.max(0.1, 1 - currentSize / targetSize);
 
     document.getElementById('sizeVariable').textContent = currentSize + 'm';
-    document.getElementById('areaVariable').textContent = currentSize * 10 + 'm²'; // Annahme: Fläche ist das Zehnfache der Größe
-    document.getElementById('soundPowerVariable').textContent = currentSize * 2 + 'dB'; // Annahme: Schalleistung ist das 1,4-Fache der Größe
-    document.getElementById('frequencyRangeVariable').textContent = currentSize * 100 + 'Hz'; // Annahme: Frequenzbereich ist das Hundertfache der Größe
-    document.getElementById('bluetoothPowerVariable').textContent = currentSize * 10 + 'W'; // Annahme: Frequenzbereich ist das Hundertfache der Größe
+    document.getElementById('areaVariable').textContent = currentSize * 10 + 'm²'; 
+    document.getElementById('soundPowerVariable').textContent = currentSize * 2 + 'dB'; 
+    document.getElementById('frequencyRangeVariable').textContent = currentSize * 100 + 'Hz'; 
+    document.getElementById('bluetoothPowerVariable').textContent = currentSize * 10 + 'W'; 
 
   }
 
   if (currentSize < targetSize && animationRunning) {
-    // Führe die nächste Aktualisierung in der nächsten Frame-Anfrage aus
+ 
     requestAnimationFrame(animateSize);
   }
 }
@@ -239,26 +232,26 @@ function animateSize() {
 
 
 
-// Erstelle einen Intersection Observer
+
 const observer = new IntersectionObserver((entries) => {
-  // Überprüfe, ob die Tabelle im sichtbaren Bereich ist
+
   if (entries[0].isIntersecting) {
-    // Starte die Animation
+
     animationRunning = true;
     animateSize();
   } else {
-    // Stoppe die Animation, wenn die Tabelle außerhalb des sichtbaren Bereichs ist
+    
     animationRunning = false;
   }
 });
 
-// Beobachte die Tabelle
+
 const table = document.querySelector('table');
 observer.observe(table);
 
 
 
-// Überprüfen, ob das Bild im sichtbaren Bereich ist
+
 function isElementInViewport(el) {
   var rect = el.getBoundingClientRect();
   return (
@@ -269,7 +262,7 @@ function isElementInViewport(el) {
   );
 }
 
-// Überprüfen Sie beim Scrollen, ob das Bild angezeigt werden soll
+
 function checkTableImageVisibility() {
   var tableImage = document.querySelector(".table-image");
   if (isElementInViewport(tableImage)) {
@@ -277,10 +270,10 @@ function checkTableImageVisibility() {
   }
 }
 
-// Fügen Sie einen Event-Listener für das Scroll-Ereignis hinzu
+
 window.addEventListener("scroll", checkTableImageVisibility);
 
-// Überprüfen Sie die Sichtbarkeit des Bildes, wenn die Seite geladen wird
+
 window.addEventListener("DOMContentLoaded", checkTableImageVisibility);
 
 
@@ -296,7 +289,7 @@ window.addEventListener("DOMContentLoaded", checkTableImageVisibility);
 const slides = document.querySelectorAll('.slider-image');
 const leftArrow = document.querySelector('.left-arrow');
 const rightArrow = document.querySelector('.right-arrow');
-let currentIndex = 0; // Initialize the current index to 0
+let currentIndex = 0; 
 
 function swapPositions(slide1, slide2) {
   slide1.style.transform = 'scale(0.72)';
@@ -328,7 +321,7 @@ function showSlide() {
 
 function nextSlide() {
   if (slides.length <= 1) {
-    return; // Do not proceed if there is only one slide
+    return; 
   }
   
   const nextIndex = (currentIndex + 1) % slides.length;
@@ -339,7 +332,7 @@ function nextSlide() {
 
 function previousSlide() {
   if (slides.length <= 1) {
-    return; // Do not proceed if there is only one slide
+    return; 
   }
   
   const prevIndex = (currentIndex - 1 + slides.length) % slides.length;
@@ -351,14 +344,14 @@ function previousSlide() {
 leftArrow.addEventListener('click', previousSlide);
 rightArrow.addEventListener('click', nextSlide);
 
-// Initially show the appropriate slide based on screen size
+
 function updateSlideDisplay() {
   const screenWidth = window.innerWidth;
   if (screenWidth < 958) {
     showSlide();
   } else {
     slides.forEach((slide) => {
-      slide.style.display = 'block'; // Show all slides when screen size is larger
+      slide.style.display = 'block'; 
     });
   }
 }
@@ -424,3 +417,12 @@ window.addEventListener("scroll", function() {
     }
   });
 });
+
+
+
+
+
+
+
+
+
